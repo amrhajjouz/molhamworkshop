9cb57f65332332f1d85b31a1662719da250857a9
AmrHajjouz <amr.hajjouz@gmail.com>
2021-03-08 14:07:41 +0200

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -144,7 +144,7 @@
         var appDebug = {{ env('APP_DEBUG') }};
         var routes = JSON.parse(("{{ $routes->toJson() }}").replace(/&quot;/g,'"'));
         var app = angular.module("app", ["ngRoute"]);
-        alert(appUrl);
+        
         app.run(function ($rootScope, $location, $page, $compile, $timeout) {
             
             $rootScope.$location = $location;
