7d9e2adcd572719a2dae00a37c294da4d1b4760f
AmrHajjouz <amr.hajjouz@gmail.com>
2021-03-08 11:00:44 +0200

    Upgrading to Laravel 8

--- public/ng/controllers/basics/notFoundController.js
+++
@@ -1,8 +0,0 @@
-function notFoundControllerInit ($page) {
-    $page.set({title : 'Not Found', hasHeader : false, alignItemsCenter : true, sidenavHidden : true});
-    return ;
-}
-
-function notFoundController ($scope, $rootScope, $http, $init) {
-
-}
--- public/ng/controllers/users/listUsersController.js
+++
@@ -1,7 +0,0 @@
-function listUsersControllerInit ($apiRequest) {
-    return $apiRequest.config('users').getData();
-}
-
-function listUsersController ($scope, $init) {
-    $scope.users = $init;
-}
