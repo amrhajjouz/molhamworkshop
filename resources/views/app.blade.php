8302e9ab2a509073b45de85941b8890986343cca
AmrHajjouz <amr.hajjouz@gmail.com>
2021-03-08 14:31:04 +0200

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -96,7 +96,7 @@
         </div>
     </nav>
 
-    <div id="page-content" class="main-content py-6 d-none">
+    <div id="page-content" class="main-content py-md-6 py-3 d-none">
         <div class="row justify-content-center mx-0">
             <div class="col-lg-11" ng-view></div>
         </div>
