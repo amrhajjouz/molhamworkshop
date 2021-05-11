2ff636ed10a2e4b3d2670607fca1ceeec74f96f2
mohamd222 <mohamd.pc2000@gmail.com>
2021-05-12 02:20:54 +0300

    review part 3

--- app/Http/Requests/Target/Event/UpdateRequest.php
+++ app/Http/Requests/Target/Event/UpdateRequest.php
@@ -27,7 +27,7 @@ class UpdateRequest extends BaseRequest
     {
         return [
             'id' => ['required', 'exists:events'],
-            'date' => ['required','after:today'],
+            'date' => ['required'],
             'public_visibility' => ['required', 'boolean'],
             'donor_id' => ['required', 'numeric'],
             'verified' => ['nullable'],
