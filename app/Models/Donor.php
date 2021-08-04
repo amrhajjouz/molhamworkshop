347311c52dd315c38bea63e8d105bae3ec777a0e
Mohamd <45457219+mohamd222@users.noreply.github.com>
2021-08-04 12:58:44 +0300

    code review

--- app/Models/Donor.php
+++ app/Models/Donor.php
@@ -5,28 +5,30 @@ namespace App\Models;
 use App\Traits\Tokenable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\{DonorNotificationPreference , NotificationPreference};
+use App\Models\{NotificationPreference};
 
 class Donor extends Model
 {
     use Tokenable;
-    
-    protected  $fillable = ["name","phone","password","email","swish_number","whatsapp_number", 'theme_mode' , 'theme_color' , 'locale' , 'currency' , 'country_code'];
+
+    protected  $fillable = ["name", "phone", "password", "email", "swish_number", "whatsapp_number", 'theme_mode', 'theme_color', 'locale', 'currency', 'country_code'];
     protected $hidden = [
-        'password', 
+        'password',
     ];
 
-    public function country() {
-        return $this->hasOne('App\Models\Country' , 'code' , 'country_code');
+    public function country()
+    {
+        return $this->hasOne('App\Models\Country', 'code', 'country_code');
     }
-   
-    public function notification_preferences() {
-        return $this->belongsToMany(NotificationPreference::class, 'donor_notification_preferences', 'donor_id' , 'preference_id');
+
+    public function notification_preferences()
+    {
+        return $this->belongsToMany(NotificationPreference::class, 'donor_notification_preferences', 'donor_id', 'preference_id');
     }
-    
-    public function delete() {
+
+    public function delete()
+    {
         $this->deleteAllTokens();
         return parent::delete();
     }
 }
-
