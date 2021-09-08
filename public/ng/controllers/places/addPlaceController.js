80cfd38f9e3f36c63150ea1d084beb022a2cba83
AmrHajjouz <amr.hajjouz@gmail.com>
2021-09-08 15:01:41 +0300

    changes in ui

--- public/ng/controllers/places/addPlaceController.js
+++ public/ng/controllers/places/addPlaceController.js
@@ -1,32 +1,23 @@
 async function addPlaceControllerInit($apiRequest) {
 
-  return {
-    countries: await $apiRequest.config("countries").getData(),
-    places: await $apiRequest.config("places").getData(),
-  };
+    return {
+        countries: await $apiRequest.config("countries").getData(),
+    };
 }
 
 function addPlaceController($scope, $location, $apiRequest, $page, $init) {
-  $scope.types = [{ id: "province", name: "محافظة" }, { id: "city", name: "مدينة" }, { id: "district", name: "منطقة" }, { id: "village", name: "قرية" },];
-  $scope.place = { name: { ar: null, en: null }, parent_id: null, type: "province", country_code: null, parent_type: null, };
-  $scope.places = $init.places;
-  $scope.countries = $init.countries;
-  $scope.parentPlaces = [];
-
-  $scope.handleChangeParentType = async (type) => {
-    $scope.parentPlaces = [];
-    let url;
-    if (type && type == "city") url = `places/search?type=province`;
-    else url = `places/search?type=${$scope.place.parent_type}`;
     
-    $scope.parentPlaces = await $apiRequest.config(url).getData();
-    $scope.$evalAsync();
-  };
-
-  $scope.handleChangeType = async () => {
-    $scope.createPlace.errors.type = null;
-    if ($scope.place.type == "city") $scope.handleChangeParentType("city");
-  };
-
-  $scope.createPlace = $apiRequest.config({ method: "POST", url: "places", data: $scope.place }, function (response, data) { $page.navigate("places.overview", { id: data.id }); });
+    $scope.place = {};
+    $scope.countries = $init.countries;
+    $scope.parentPlaces = [];
+    
+    $scope.createPlace = $apiRequest.config({
+        method: "POST",
+        url: "places",
+        data: $scope.place
+    }, function (response, data) {
+        $page.navigate("places.overview", {
+            id: data.id
+        });
+    });
 }
--- public/ng/controllers/places/editPlaceController.js
+++ public/ng/controllers/places/editPlaceController.js
@@ -1,39 +1,18 @@
 async function editPlaceControllerInit($http, $page, $apiRequest) {
-  const place = await $apiRequest.config("places/" + $page.routeParams.id).getData();
-  const countries = await $apiRequest.config("countries").getData();
-  let url = "places/search";
-  if (place.parent) {
-    place.parent_type = place.parent.type;
-    url += "?type=" + place.parent.type;
-  }
-  const parentPlaces = await $apiRequest.config(url).getData();
-  
-  return {
-    place: place,
-    countries: countries,
-    parentPlaces: parentPlaces,
-  };
+    const place = await $apiRequest.config("places/" + $page.routeParams.id).getData();
+    
+    return {
+        place: place,
+    };
 }
 
 function editPlaceController($scope, $page, $apiRequest, $init) {
-  $scope.types = [{ id: "province", name: "محافظة" }, { id: "city", name: "مدينة" }, { id: "district", name: "منطقة" }, { id: "village", name: "قرية" },];
-  $scope.place = $init.place;
-  $scope.countries = $init.countries;
-  $scope.parentPlaces = $init.parentPlaces;
+    
+    $scope.place = $init.place;
 
-  $scope.handleChangeParentType = async (type) => {
-    $scope.parentPlaces = [];
-    let url;
-    if (type && type == "city") url = `places/search?type=province`;
-    else url = `places/search?type=${$scope.place.parent_type}`;
-    $scope.parentPlaces = await $apiRequest.config(url).getData();
-    $scope.$evalAsync();
-  };
-
-  $scope.handleChangeType = async () => {
-    $scope.createPlace.errors.type = null;
-    if ($scope.place.type == "city") $scope.handleChangeParentType("city");
-  };
-
-  $scope.updatePlace = $apiRequest.config({ method: "PUT", url: "places", data: $scope.place, }, function (response, data) { });
+    $scope.updatePlace = $apiRequest.config({
+        method: "PUT",
+        url: "places",
+        data: $scope.place,
+    }, function (response, data) {});
 }
