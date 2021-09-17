ac862724c90ef795dc4cd9534de77c87c9a6554f
AmrHajjouz <amr.hajjouz@gmail.com>
2021-09-17 11:06:43 +0300

    cities seeder init

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,36 @@ use App\Http\Controllers\Dashboard\SpaController;
 
 require __DIR__.'/auth.php';
 
+Route::get('/cities_seeder', function () {
+    
+    $csv = collect(getCombinedCsv (base_path('/init_data/worldcities.csv')));
+    $places = [];
+    
+    foreach ($csv as $i => $c) {
+        $places[($c['capital'] == 'admin' || $c['capital'] == 'primary') ? $c['admin_name'] : $c['city']] = array_merge($c, ['id' => $i+1, 'parent_id' => null]);
+    }
+    
+    $places = collect($places);
+    
+    /*foreach ($places as $i => $c) {
+        if ($c['capital'] == 'minor') {
+            $places[$i]['parent_id'] = (isset($places[$c['admin_name']])) ? $places[$c['admin_name']]['id'] : null;
+        }
+    }*/
+    
+    
+    $places = $places->map(function ($item, $key) use ($places) {
+        if ($item['capital'] == 'minor') {
+            //if ($key == 'Jablah');
+            $item['parent_id'] = (isset($places[$item['admin_name']])) ? $places[$item['admin_name']]['id'] : null;
+        }
+        return $item;
+    });    
+
+    dd($places->where('iso2', 'SY'));
+    
+    return ;    
+    
+});
+
 Route::middleware('auth')->get('{url?}', [SpaController::class, 'index'])->where('url', '.*')->name('home');
