e2e4271083c80c8d464d22895247e58ddbcd7c30
AmrHajjouz <amr.hajjouz@gmail.com>
2021-11-04 16:42:59 +0200

    fix header issue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1756,8 +1756,7 @@
     </div>
     <!-- / 2nd Filters Modal -->
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
     <script src="{{ asset('js/theme.js?v=7') }}"></script>
     <!-- <script src="{{ asset('js/custom.js') }}"></script> -->
